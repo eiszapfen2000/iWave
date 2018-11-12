ca2663132c5f52b4ba800b8697ac252c16a61120
Thomas Gamper <thomas.gamper@zenotrack.com>
2018-11-12 15:31:08 +0100

    - G_zero.m simplify

--- G_zero.m
+++ G_zero.m
@@ -1,9 +1,6 @@
-function x = G_zero(n, deltaQ)
-
+function G_0 = G_zero(n, deltaQ, sigma)
+%%
+if nargin < 3, sigma = 1; end
 q = deltaQ:deltaQ:n*deltaQ;
-qSquare = q.*q;
-powers = exp(-1 * qSquare);
-scalars = qSquare .* powers;
-x = sum(scalars);
-
+G_0 = sum(q.*q.*exp(-sigma.*q.*q));
 end
